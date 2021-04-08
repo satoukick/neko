ade2f15d19b74ca355afdd75cf657654bbec3895
cyw <weems1995@hotmail.com>
2021-04-08 23:52:05 +0800

    read standard output when FILE is -

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // TODO: read from standard input with no FILE input or "-"
     // TODO: write test files!
     if args.len() == 1 {
         println!("running in echo mode!");
@@ -14,6 +13,12 @@ fn main() {
     }
 
     let filename = &args[1];
+
+    if filename == "-" {
+        println!("running in echo mode!");
+        read::echo_mode().expect("echo mode error");
+        return;
+    }
 
     read::read_file_lines(filename.to_string());
 }
